f51875d83f3b5c19d9844dab754af594d3b9c617
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-05-21 12:08:51 +0700

    Add Hero component and test React.memo

--- src/App.js
+++ src/App.js
@@ -1,100 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.scss";
-import Pagination from "./components/Pagination";
-import PostFilterForm from "./components/PostFilterForm";
-import PostList from "./components/PostList";
-import queryString from "query-string";
-import Clock from "./components/Clock";
-import MagicBox from "./components/MagicBox";
+import Hero from "./components/Hero";
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 1, title: "Football" },
-    { id: 2, title: "Tennis" },
-    { id: 3, title: "Volleyball" },
-  ]);
-  const [postList, setPostList] = useState([]);
-  const [pagination, setPagination] = useState({
-    _page: 1,
-    _limit: 10,
-    _totalRows: 10,
-  });
-  const [filters, setFilters] = useState({
-    _limit: 10,
-    _page: 1,
-  });
-  const [showClock, setShowClock] = useState(true);
-
-  useEffect(() => {
-    async function fetchPostList() {
-      try {
-        const paramsString = queryString.stringify(filters);
-        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
-        const response = await fetch(requestUrl);
-        const responseJSON = await response.json();
-        console.log({ responseJSON });
-
-        const { data, pagination } = responseJSON;
-        setPostList(data);
-        setPagination(pagination);
-      } catch (error) {
-        console.log("Failed to fetch post list: ", error.message);
-      }
-    }
-    fetchPostList();
-  }, [filters]);
-
-  const handlePageChange = (newPage) => {
-    console.log("New page: ", newPage);
-    setFilters({
-      ...filters,
-      _page: newPage,
-    });
-  };
-
-  const handleTodoClick = (todo) => {
-    console.log(todo);
-    const index = todoList.findIndex((x) => x.id === todo.id);
-    if (index < 0) return;
-
-    const newTodoList = [...todoList];
-    newTodoList.splice(index, 1);
-    setTodoList(newTodoList);
-  };
-
-  const handleTodoFormSubmit = (formValues) => {
-    console.log(formValues);
-    // add new todo to current todo list
-    const newTodo = {
-      id: todoList.length + 1,
-      ...formValues,
-    };
-    const newTodoList = [...todoList];
-    newTodoList.push(newTodo);
-    setTodoList(newTodoList);
-  };
-
-  const handleFiltersChange = (newFilters) => {
-    console.log("New filters: ", newFilters);
-    setFilters({
-      ...filters,
-      _page: 1,
-      title_like: newFilters.searchTerm,
-    });
-  };
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <h1>Welcome to React Hooks</h1>
-      <MagicBox />
-      {/* {showClock && <Clock />}
-      <button onClick={() => setShowClock(false)}>Hide Clock</button> */}
-      {/* <PostFilterForm onSubmit={handleFiltersChange} />
-      <PostList posts={postList} />
-      <Pagination pagination={pagination} onPageChange={handlePageChange} /> */}
-      {/* <ColorBox />
-      <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+
+      <Hero name="Curtis" />
     </div>
   );
 }
